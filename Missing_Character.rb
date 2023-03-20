a47e9a42861080606f98b9439961098598da82e0
Edmilson-Lopes <milsonlopes@hotmail.com>
2023-03-20 20:53:23 -0300

    Problema-String Missing Character

---
+++ Missing_Character.rb
@@ -0,0 +1,30 @@
+
+#Missing Character
+
+MAX_CHAR = 26
+
+def missing_chars(str)
+
+    present = Array.new(MAX_CHAR, false)
+
+    str.each_char do |char|
+        if char.match?(/[a-z]/)
+            present[char.ord - 'a'.ord] = true
+        elsif char.match?(/[A-Z]/)
+            present[char.ord - 'A'.ord] = true
+        end
+    end
+
+    res = ""
+    present.each_with_index do |val, index|
+        if !val
+            res += (index + 'a'.ord).chr 
+        end 
+    end
+
+    return res
+end
+
+str = "The quick brown fox jumps over the dog"
+
+p missing_chars(str)
