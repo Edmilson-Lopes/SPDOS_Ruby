d5d273af323c0e984b1e1b9f378f1a36a3494e93
Edmilson-Lopes <milsonlopes@hotmail.com>
2024-02-12 22:03:11 -0300

    Algoritmos fase 01

---
+++ Algoritmos de fase 1/Recap/Linear_Search2.rb
@@ -0,0 +1,27 @@
+
+#Linear Search 
+
+def search(array, numero_alvo)
+
+    for index in 0..array.length-1 do 
+        if array[index] == numero_alvo 
+            return index 
+        end 
+    end
+
+    return -1 
+end
+
+array = [5, 12, 15, 20, 25, 33, 41, 50]
+
+numero_alvo = gets.chomp.to_i 
+
+resultado = search(array, numero_alvo)
+
+if resultado == -1
+    p "O número alvo não pode ser encontrado"
+
+else
+    p "O número alvo foi encontrado"
+end
+
