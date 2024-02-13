1981b6863e8788fbae7816b480918a1be00c5ade
Edmilson-Lopes <milsonlopes@hotmail.com>
2024-02-12 22:19:12 -0300

    Algoritmos fase 01

---
+++ Algoritmos de fase 1/Recap/Binary_search2.rb
@@ -0,0 +1,37 @@
+
+arr = [5, 10, 12, 18, 25]
+
+print "Digite o Alvo: "
+target = gets.chomp.to_i
+
+def binary_search(arr, target)
+
+    left = 0
+    right = arr.length-1 
+
+    while left <= right 
+        mid = (left + right) / 2
+
+        if arr[mid] == target 
+            return mid 
+
+        elsif arr[mid] < target 
+            left = mid +1
+
+        else arr[mid] > target
+            right = mid -1
+        end 
+    end
+
+    return -1 
+end
+
+resultado = binary_search(arr, target)
+
+if resultado == -1 
+    p "O número não foi encontrado na lista"
+
+else
+    p "o número foi encontrado na lista"
+end
+
